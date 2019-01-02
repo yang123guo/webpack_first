74341942733a99f3462e1736c050ead69e4657e2
yanggqm <289656703@qq.com>
2019-01-02 23:47:05 +0800

    yang

--- webpack.config.js
+++ webpack.config.js
@@ -101,18 +101,18 @@ console.log('当前环境是开发环境吗？：', devMode ,  'env.mode是什�
             new CleanWebpackPlugin(['dist'])
         ],
 
-        // optimization: {
-        //     minimizer: [
-        //         new UglifyJsPlugin({
-        //             // cache: true,
-        //             // parallel: true,
-        //             // sourcMap: true
-        //         }),
-        //         new OptimizeCSSAssetsPlugin({
+        optimization: {
+            minimizer: [
+                new UglifyJsPlugin({
+                    // cache: true,
+                    // parallel: true,
+                    // sourcMap: true
+                }),
+                new OptimizeCSSAssetsPlugin({
                     
-        //         }),
-        //     ],
-        // }
+                }),
+            ],
+        }
     };
    
 }
